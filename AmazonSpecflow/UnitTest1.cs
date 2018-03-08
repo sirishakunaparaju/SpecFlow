9d90de219f1150f9eb6c7c46deab525ebda0d7fa
skunaparaju <skunaparaju@UK03-102369>
2018-03-08 10:54:31 +0000

    Add project files.

---
+++ AmazonSpecflow/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AmazonSpecflow
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
